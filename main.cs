23d21d4b89c677a2202d8bd8aac019efb4560bc6
Gleb Renkas <102474465+Flopperdvlp@users.noreply.github.com>
2024-11-24 12:56:16 +0200

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,11 +1,28 @@
-using System
-namespace Main
+using System;
+using System.IO;
+
+namespace Program
 {
-	public class Program
-	{
-		private void Main()
-		{
-			Console.WriteLine("Hello world!");
-		}
-	}
+    public class Program
+    {
+        private static string Path = "D:\\Desktop\\User.sql";
+
+        public static void Main(string[] args)
+        {
+            Show(Path);
+        }
+
+        public static void Show(string path)
+        {
+            if (File.Exists(path))
+            {
+                string content = File.ReadAllText(path);
+                Console.WriteLine(content);
+            }
+            else
+            {
+                Console.WriteLine("Файл не найден: " + path);
+            }
+        }
+    }
 }
